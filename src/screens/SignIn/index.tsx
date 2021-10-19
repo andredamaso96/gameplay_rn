94b001ca389753d48435ed379eaef63a9c38d23e
Vieira Borges <adamaso@t-systems.es>
2021-10-19 16:05:56 +0100

    first commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { 
+    View, 
+    Text, 
+    Image,
+    StatusBar 
+} from "react-native";
+
+import IllustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { styles } from "./styles";
+
+export const SignIn = () => {
+    
+    return (
+       <View style={styles.container}>
+           <StatusBar 
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+           />
+           <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+
+           <View style={styles.content}>
+               <Text style={styles.title}>
+                    Organize {'\n'}
+                    seus jogos {'\n'}
+                    facilmente
+               </Text>
+
+               <Text style={styles.subtitle}>
+                   Crie grupos para jogar os jogos {'\n'}
+                   favoritos com seus amigos
+               </Text>
+
+               <ButtonIcon title="Entrar com Discord" activeOpacity={0.7}/>
+           </View>
+       </View>
+    );
+}
